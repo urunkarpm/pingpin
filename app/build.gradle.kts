25ba21438054b4dde60f64ccb33c4e04d94b13bc
urunkarpm <uprasenjeet@gmail.com>
2026-08-19 20:43:08 +0530

    release: v1.7.0 - Clarified local Wi-Fi calendar tracking vs HR portal check-in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.urunkarpm.pingpin"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.6.0"
+        versionCode = 7
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
